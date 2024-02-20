62a42735a78e14f9310f94132e0f9e27cbdd692f
Mimi <fwfurtado@gmail.com>
2024-02-19 21:53:18 -0300

    Revert "feat: add regreet"
    This reverts commit 2f6b041b86bde5890dce62511acd86920cf2b9be.

--- modules/nixos/settings/display-manager.nix
+++ modules/nixos/settings/display-manager.nix
@@ -1,35 +1,16 @@
-{ pkgs, lib, config, ... }:
-let
-  userConfig = config.home-manager.users.me;
-  # gtkTheme = userConfig.gtk.theme;
-  # iconTheme = userConfig.gtk.iconTheme;
-  # wallpaper = userConfig.wallpaper;
-
-in {
+{ pkgs, ... }: {
   # Enable Display Manager
   # services.xserver.displayManager.sddm.enable = true;
   # services.xserver.desktopManager.gnome.enable = true;
 
    # Enable Display Manager
-    programs.regreet = {
-      enable = true;
-      # settings = {
-      #   GTK = {
-      #     icon_theme_name = "Papirus";
-      #     # theme_name = gtkTheme.name;
-      #   };
-      #   background = {
-      #     path = wallpaper;
-      #     fit = "Cover";
-      #   };
-      # };
-    };
+    programs.regreet.enable = true;
 
     services.greetd = {
       enable = true;
       settings = {
         default_session = {
-          command = "${lib.getExe config.programs.regreet.package}";
+          command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --time-format '%I:%M %p | %a • %h | %F' --cmd Hyprland";
           user = "me";
         };
       };
