2f6b041b86bde5890dce62511acd86920cf2b9be
Mimi <fwfurtado@gmail.com>
2024-02-19 21:48:44 -0300

    feat: add regreet

--- modules/nixos/settings/display-manager.nix
+++ modules/nixos/settings/display-manager.nix
@@ -1,16 +1,35 @@
-{ pkgs, ... }: {
+{ pkgs, lib, config, ... }:
+let
+  userConfig = config.home-manager.users.me;
+  # gtkTheme = userConfig.gtk.theme;
+  # iconTheme = userConfig.gtk.iconTheme;
+  # wallpaper = userConfig.wallpaper;
+
+in {
   # Enable Display Manager
   # services.xserver.displayManager.sddm.enable = true;
   # services.xserver.desktopManager.gnome.enable = true;
 
    # Enable Display Manager
-    programs.regreet.enable = true;
+    programs.regreet = {
+      enable = true;
+      # settings = {
+      #   GTK = {
+      #     icon_theme_name = "Papirus";
+      #     # theme_name = gtkTheme.name;
+      #   };
+      #   background = {
+      #     path = wallpaper;
+      #     fit = "Cover";
+      #   };
+      # };
+    };
 
     services.greetd = {
       enable = true;
       settings = {
         default_session = {
-          command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --time-format '%I:%M %p | %a • %h | %F' --cmd Hyprland";
+          command = "${lib.getExe config.programs.regreet.package}";
           user = "me";
         };
       };
